b26bed733d487617ef7b4ff4cf12941026071661
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-03-27 14:19:44 -0300

    converted mapview coffee to js

---
+++ packages/rocketchat-mapview/client/mapview.js
@@ -0,0 +1,20 @@
+
+/*
+* MapView is a named function that will replace geolocation in messages with a Google Static Map
+* @param {Object} message - The message object
+*/
+
+const MapView = function(message) {
+	const mv_googlekey = RocketChat.settings.get('MapView_GMapsAPIKey');
+	if (message.location) {
+		const [longitude, latitude] = message.location.coordinates;
+		if (mv_googlekey && mv_googlekey.length) {
+			message.html = `<a href="https://maps.google.com/maps?daddr=${ latitude },${ longitude }" target="_blank"><img src="https://maps.googleapis.com/maps/api/staticmap?zoom=14&size=250x250&markers=color:gray%7Clabel:%7C${ latitude },${ longitude }&key=${ mv_googlekey }" /></a>`;
+		} else {
+			message.html = `<a href="https://maps.google.com/maps?daddr=${ latitude },${ longitude }" target="_blank">${ TAPi18n.__('Shared_Location') }</a>`;
+		}
+	}
+	return message;
+};
+
+RocketChat.callbacks.add('renderMessage', MapView, RocketChat.callbacks.priority.HIGH);
